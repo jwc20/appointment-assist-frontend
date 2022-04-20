dae3e481f83b3d6729eb9a86d5012c43c5417d20
jwc20 <choij2015@gmail.com>
2022-04-20 10:04:12 -0700

    Added basic table for listing patients

--- src/pages/Patients.js
+++ src/pages/Patients.js
@@ -1,34 +1,24 @@
-
 import { useEffect, useState } from "react";
+import PatientList from "../components/PatientList";
 
 const BASE_URL = "http://localhost:9292/";
 
 function Patients() {
-
   const [patients, setPatients] = useState([]);
 
   useEffect(() => {
     fetch(BASE_URL + "patients")
-      .then(r => r.json())
-      .then(patients => setPatients(patients))
+      .then((r) => r.json())
+      .then((patients) => setPatients(patients));
   }, []);
 
   return (
     <div>
       <p>This is the Patients Page</p>
 
-      {/*
-        {patients.map(() => (
-        <PatientList key={patient.id} patient={patient} />
-        ))}
-      */}
-
-
-
-
+      <PatientList patients={patients} />
     </div>
-  )
-
+  );
 }
 
-export default Patients
+export default Patients;
