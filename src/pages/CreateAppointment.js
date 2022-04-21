05b3a59c49f2d86bf47c43d9c5c0946ec678de30
jwc20 <choij2015@gmail.com>
2022-04-21 09:39:06 -0700

    Fixed unexpected end of JSON input error

--- src/pages/CreateAppointment.js
+++ src/pages/CreateAppointment.js
@@ -3,23 +3,24 @@ import { useEffect, useState } from "react";
 const BASE_URL = "http://localhost:9292/";
 
 function CreateAppointment({ onAddAppointment }) {
-  const [appointmentDate, setAppointmentDate] = useState("");
-  const [appointmentDuration, setAppointmentDuration] = useState("");
+  const [newAppointment, setNewAppointment] = useState([]);
+  const [appointment_date, setAppointmentDate] = useState("");
+  const [appointment_duration, setAppointmentDuration] = useState(0);
   const [appointment_type, setAppointmentType] = useState("");
-  const [appointmentReason, setAppointmentReason] = useState("");
+  const [appointment_reason, setAppointmentReason] = useState("");
 
-  const [doctorLastName, setDoctorLastName] = useState("");
-  const [doctorFirstName, setDoctorFirstName] = useState("");
-  const [doctorPhone, setDoctorPhone] = useState("");
-  const [doctorEmail, setDoctorEmail] = useState("");
+  // const [doctorLastName, setDoctorLastName] = useState("");
+  // const [doctorFirstName, setDoctorFirstName] = useState("");
+  // const [doctorPhone, setDoctorPhone] = useState("");
+  // const [doctorEmail, setDoctorEmail] = useState("");
 
-  const [patientLastName, setPatientLastName] = useState("");
-  const [patientFirstName, setPatientFirstName] = useState("");
-  const [patientPhone, setPatientPhone] = useState("");
-  const [patientEmail, setPatientEmail] = useState("");
-  const [patientAddress, setPatientAddress] = useState("");
-  const [patientCity, setPatientCity] = useState("");
-  const [patientState, setPatientState] = useState("");
+  // const [patientLastName, setPatientLastName] = useState("");
+  // const [patientFirstName, setPatientFirstName] = useState("");
+  // const [patientPhone, setPatientPhone] = useState("");
+  // const [patientEmail, setPatientEmail] = useState("");
+  // const [patientAddress, setPatientAddress] = useState("");
+  // const [patientCity, setPatientCity] = useState("");
+  // const [patientState, setPatientState] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -29,39 +30,76 @@ function CreateAppointment({ onAddAppointment }) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        appointmentDate: appointmentDate,
-        appointmentDuration: appointmentDuration,
         appointment_type: appointment_type,
-        appointmentReason: appointmentReason,
-        doctorLastName: doctorLastName,
-        doctorFirstName: doctorFirstName,
-        doctorPhone: doctorPhone,
-        doctorEmail: doctorEmail,
-        patientLastName: patientLastName,
-        patientFirstName: patientFirstName,
-        patientPhone: patientPhone,
-        patientEmail: patientEmail,
-        patientAddress: patientAddress,
-        patientCity: patientCity,
-        patientState: patientState,
+        appointment_date: appointment_date,
+        appointment_duration: appointment_duration,
+        appointment_reason: appointment_reason,
+        // doctorLastName: doctorLastName,
+        // doctorFirstName: doctorFirstName,
+        // doctorPhone: doctorPhone,
+        // doctorEmail: doctorEmail,
+        // patientLastName: patientLastName,
+        // patientFirstName: patientFirstName,
+        // patientPhone: patientPhone,
+        // patientEmail: patientEmail,
+        // patientAddress: patientAddress,
+        // patientCity: patientCity,
+        // patientState: patientState,
         // doctor_id: doctor_id,
         // patient_id: patient_id,
       }),
     })
-      .then((r) => r.json())
-      .then((newAppointment) => onAddAppointment(newAppointment));
+      // https://stackoverflow.com/questions/50048785/unexpected-end-of-json-input-error
+      // .then((r) => r.json())
+      // .then((newAppointment) => onAddAppointment(newAppointment));
+      .then((newAppointment) => setNewAppointment(newAppointment));
+    console.log("New appointment made.");
   }
 
   function handleAppointmentTypeChange(e) {
     setAppointmentType(e.target.value);
   }
 
+  function handleAppointmentDateChange(e) {
+    setAppointmentDate(e.target.value);
+  }
+
+  function handleAppointmentDurationChange(e) {
+    setAppointmentDuration(e.target.value);
+  }
+
+  function handleAppointmentReasonChange(e) {
+    setAppointmentReason(e.target.value);
+  }
 
   return (
     <div>
       <p>Hello </p>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleAppointmentTypeChange} value={appointment_type} />
+        <input
+          type="text"
+          onChange={handleAppointmentTypeChange}
+          value={appointment_type}
+          placeholder="type"
+        />
+        <input
+          type="date"
+          onChange={handleAppointmentDateChange}
+          value={appointment_date}
+          placeholder="date"
+        />
+        <input
+          type="number"
+          onChange={handleAppointmentDurationChange}
+          value={appointment_duration}
+          placeholder="duration"
+        />
+        <input
+          type="text"
+          onChange={handleAppointmentReasonChange}
+          value={appointment_reason}
+          placeholder="reason"
+        />
 
         <button type="submit">Submit</button>
       </form>
